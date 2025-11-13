cde707ed1ed23e5a187a96383483ccfe100256d5
crispifx <144757931+crispifx@users.noreply.github.com>
2025-11-12 23:51:11 -0300

    Add files via upload
    Agrego los scripts y las imagenes correspondientes a la pagina web del producto SmartPot

---
+++ script.js
@@ -0,0 +1,27 @@
+// Boton de Contacto
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const contactForm = document.getElementById("contact-form");
+
+    contactForm.addEventListener("submit", function(event) {
+        
+        event.preventDefault();
+
+        alert("✅ Mensaje enviado con éxito. Gracias por tu interés. Nos pondremos en contacto pronto.");
+
+        contactForm.reset();
+    });
+
+});
+
+// Boton de Compra
+
+document.addEventListener('DOMContentLoaded', () => {
+    const botonCompra = document.querySelector('.btn-comprar');
+  
+    botonCompra.addEventListener('click', () => {
+      alert('¡Gracias por comprar el producto! Tu pedido se está procesando.');
+    });
+  });
+  
